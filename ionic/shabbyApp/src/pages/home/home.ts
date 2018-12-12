d7197abfbe417ad9c158f07ce9e22d2df9c46bbb
woutervu <wouwousoft@gmail.com>
2018-12-12 22:41:21 +0100

    Working dummy data.

--- ionic/shabbyApp/src/pages/home/home.ts
+++ ionic/shabbyApp/src/pages/home/home.ts
@@ -6,9 +6,49 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  devices = [
+    {
+      "id": "40cf0c30-1d98-4b5b-8814-51d112343b58",
+      "name": "Desk light",
+      "status": true
+    },
+    {
+      "id": "a38641e5-ecdc-44f1-a414-1119c5a0ac78",
+      "name": "Living room light",
+      "status": false
+    },
+    {
+      "id": "fe4b2538-fefb-4470-bfe7-12f93e9cbf56",
+      "name": "Nightstand light",
+      "status": false
+    },
+    {
+      "id": "d716220a-3139-4b2f-9526-99d7a85cb52b",
+      "name": "Kitchen light",
+      "status": true
+    },
+    {
+      "id": "cc9aab58-e563-4d64-a516-e6359831e9af",
+      "name": "Garage light",
+      "status": false
+    }
 
-  constructor(public navCtrl: NavController) {
+  ];
 
+  constructor(public navCtrl: NavController, ) {
+
+  }
+
+  /**
+   * Toggle device status.
+   *
+   * @param uuid
+   */
+  toggleDeviceStatus(uuid) {
+    console.log("Clicked: " + uuid);
+    let index = this.devices.findIndex(i => i.id === uuid);
+    this.devices[index]['status'] = !this.devices[index]['status'];
+    console.log(this.devices[index]);
   }
 
 }
