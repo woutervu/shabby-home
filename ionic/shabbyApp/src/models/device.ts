c6fee8bbf7ca09b46428c66e5e0d507eff97da4d
woutervu <wouwousoft@gmail.com>
2018-12-18 20:40:47 +0100

    WIP object iterator in constructor.

--- ionic/shabbyApp/src/models/device.ts
+++ ionic/shabbyApp/src/models/device.ts
@@ -1,6 +1,10 @@
 export class Device {
  
-    constructor(public device: any){
- 
+    constructor(public device: any) {
+      let self = this;
+      Object.keys(device).forEach(function (value, key) {
+        // @todo: WIP find the value by the object key. #HACKERMAN
+        self[key] = value;
+      });
     }
 }
